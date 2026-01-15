c35bdb805e6142cdf2a3a4d3c8e899baa9fa366a
manbaout666999 <manbaout666999@gmail.conm>
2026-01-15 21:17:57 +0800

    SmartMedicine第一次提交

---
+++ SmartMedicine/app/src/main/java/com/example/smartmedicine/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.smartmedicine;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.example.smartmedicine.base.MedicineInfo;
+import com.example.smartmedicine.api.MedicineApi;
+import com.example.smartmedicine.api.RetrofitClient;
+import com.example.smartmedicine.util.ImageUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
